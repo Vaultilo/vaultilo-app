9aefbf96843a78497427f907afe9c34d2d496c67
Gaurav KC <13gaurab@gmail.com>
2019-12-25 14:45:58 +0545

    Add sidebar component and routes

---
+++ src/components/Sidebar.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import './Sidebar.css';
+import {Link} from 'react-router-dom';
+
+export default function Sidebar(props) {
+  const credentials = props.credentials === null ? [] : JSON.parse(props.credentials);
+  const [activeNav, setActiveNav] = useState('crypto');
+  const [activeSubNav, setActiveSubNav] = useState('');
+  const handleNavClick = (path) => {
+    setActiveNav(path);
+    setActiveSubNav('');
+  }
+  const handleSubNavClick = (path) => {
+    setActiveSubNav(path);
+  }
+  return (
+    <div className="sidenav">
+      <Link className={`dropdown-btn ${activeNav === 'crypto' ? 'active': ''}`} to="/crypto/all" onClick={() => handleNavClick('crypto')}>
+        CryptoCurrencies
+      </Link>
+      <div className="dropdown-container">
+        {[...new Set(credentials.map(cred => cred.type))].map(type => {
+          return(
+            <Link key={`crypto-${type}`} className={`${ activeSubNav === type ? 'active': ''}`} to={`/crypto/${type}`} onClick={() => handleSubNavClick(type)}>{type}</Link>
+          )
+        })}
+      </div>
+      <Link className={`dropdown-btn ${activeNav === 'search' ? 'active': ''}`} to="/search" onClick={() => handleNavClick('search')}>Search</Link>
+    </div>
+  );
+}
